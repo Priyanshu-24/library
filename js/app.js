c4ccc58484d864afcc4b31701a3166f8bcb4911c
Priyanshu Pandey <priyanshu24pandey@gmail.com>
2021-11-01 09:59:50 +0530

    Styling Done

--- js/app.js
+++ js/app.js
@@ -7,13 +7,16 @@ addBook.addEventListener("click", function (e) {
   let title = document.getElementById("title");
   let nop = document.getElementById("nop");
 
-  if(author.value == "" || title.value == "" || nop.value == "")
-  alert("Required Input Field is missing");
+  if (author.value == "" || title.value == "" || nop.value == "")
+    alert("Required Input Field is missing");
 
-  else if(nop.value == 0)
+  else if (nop.value == 0) 
   alert("Number of Pages cannot be: 0");
 
-  else{
+  else {
+    let books = JSON.parse(localStorage.getItem("library"));
+
+    if (books) library = books;
 
     library.push({ author: author.value, title: title.value, nop: nop.value });
     localStorage.setItem("library", JSON.stringify(library));
@@ -24,43 +27,35 @@ addBook.addEventListener("click", function (e) {
     title.value = "";
     nop.value = "";
   }
-
-});
+})
 
 function displayBooks() {
   let books = JSON.parse(localStorage.getItem("library"));
 
-  if(books !== null)
-  {
+  if (books !== null) {
+    let html = "";
 
-
-  let html = "";
-
-  books.forEach(function (element, index) {
-    html += `
-
-        <div class = "card">
-             <div>${element.author}</div>
-             <div>${element.title}</div>
-            <div>${element.nop}</div>
-            <button id = "${index}" onClick = "deleteBook(this.id)">Delete</button>
-        </div>
- 
+    books.forEach(function (element, index) {
+      html += `
+        <tr>
+          <th>${element.author}</th>
+          <th>${element.title}</th>
+          <th>${element.nop}</th>
+          <th><button id = "${index}" onClick = "deleteBook(this.id)" class = "dlt-btn">Delete</button></th>
+          </tr>
         `;
-  });
+    });
 
-  let a = document.getElementById("library");
-  a.innerHTML = html;
+    let a = document.getElementById("library");
+    a.innerHTML = html;
+  }
 }
 
-}
+function deleteBook(index) {
+  // console.log("deleting", index);
 
-function deleteBook(index){
-    console.log("deleting", index);
-
-    let books = JSON.parse(localStorage.getItem("library"));
-    books.splice(index,1);
-    localStorage.setItem("library", JSON.stringify(books));
-    displayBooks();
-
+  let books = JSON.parse(localStorage.getItem("library"));
+  books.splice(index, 1);
+  localStorage.setItem("library", JSON.stringify(books));
+  displayBooks();
 }
